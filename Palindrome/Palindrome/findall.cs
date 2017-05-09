b7a80a9534561389f830d9e3377d27990bb423df
ABENA-LAPTOP\Abena <abena.saulka@columbia.edu>
2017-05-09 02:07:20 -0500

    revise

---
+++ Palindrome/Palindrome/findall.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class findall
+    {
+        public static void main(string[] args)
+        {
+            FindAllPalindromes finder = new FindAllPalindromes();
+            finder.printAllPalindromes("abcddcbaABCDEDCBA");
+        }
+
+        public void printAllPalindromes(string inputText)
+        {
+            if (inputText == null)
+            {
+                Console.WriteLine("Input cannot be null!");
+                return;
+            }
+            if (inputText.length() <= 2)
+            {
+                Console.WriteLine("Minimum three characters should be present
+            }
+
+            //ODD Occuring Palindromes
+            int len = inputText.length();
+            for (int i = 1; i < len - 1; i++)
+            {
+                for (int j = i - 1, k = i + 1; j >= 0 && k < len; j--, k++)
+                {
+                    if (inputText.charAt(j) == inputText.charAt(k))
+                    {
+                        System.out.println(inputText.subSequence(j, k + 1));
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+        }
+}
---
+++ Palindrome/Palindrome/subsets.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class subsets
+    {
+        public static List<string> GetPalindromes(string source) 
+        {
+            List<string> subsets = new List<string>();
+            for (int i = 0; i < source.Length - 1; i++)
+            {
+                for (int j = i + 1; j <= source.Length; j++)
+                {
+                    if (j - i > 1 && source[j - 1] == source[i])
+                    {
+                        string currentSubset = source.Substring(i, j - i);
+                        if (IsPalindrome1(currentSubset))
+                        {
+                            subsets.Add(currentSubset);
+                        }
+                    }
+                }
+            }
+            return subsets;
+        }
+        /// <summary>
+        /// Determines whether the string is a palindrome.
+        /// </summary>
+        public static bool IsPalindrome1(string value)
+        {
+            string str = value.ToLower();
+            string reverse = new string(str.ToCharArray().Reverse().ToArray());
+            bool isPalindrome = reverse.Equals(str);
+            return isPalindrome;
+        }
+        /// <summary>
+        /// Determines whether the string is a palindrome.
+        /// </summary>
+        private static bool IsPalindromeSet(string input)
+        {
+            return !input.Where((t, i) => t != input[input.Length - 1 - i]).Any();
+        }
+    }
+}
